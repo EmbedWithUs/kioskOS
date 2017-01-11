f0d894c66f443241092fc2bd02fb5d45d461b963
Michael Large <codethebeard@users.noreply.github.com>
2017-01-11 16:05:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
+# Do initial update and upgrade
 sudo apt-get update
 sudo apt-get dist-upgrade
 
-# Setup network settings
-# /etc/network/interfaces
-
-# Enter raspi-config "sudo raspi-config"
-# Set Localization settings
-# Enable SSH
-# Expand Root Partition
-# Set Autologin on Console (B2)
-
-# Install Dependencies
-# Must have Raspbian Jessie Lite image
-# midori - Minimal, lightweight Browser
-# matchbox-window-manager - Window Manager
-# x11-xserver-utils - Interaction with hardware
-# unclutter - hide the cursor
-# xinit - allows starting X Display server.
-# fbi - FrameBuffer Image viewer. For splashscreen. https://blog.qruizelabs.com/2014/04/29/raspberrypi-kiosk-matchbox-uzbl/
+# Install operating system dependencies
 sudo apt-get install midori matchbox-window-manager x11-xserver-utils unclutter xinit fbi
 
 # Install NodeJS
@@ -32,11 +17,9 @@ npm install -g nodemon # Persistant watching of server and restart on failure.
 # Apt-Get Cleanup
 sudo apt-get clean
 
-###
-# Make it executable "sudo chmod +x /home/pi/startkiosk.sh"
-# Update /home/pi/.bashrc
-###
-# if [ -z "${SSH_TTY}" ]; then
-#   xinit ~/startkiosk.sh
-# fi
-###
+# Move script to home directory and set permissions
+mv ./startkiosk.sh /home/pi/startkiosk.sh
+sudo chmod +x /home/pi/startkiosk.sh
+
+# Move script to home directory
+mv ./.bashrc /home/pi/.bashrc
